4db1e594a521fca9a317def514c0a3e28755c494
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-26 07:16:16 +0800

    first

---
+++ society/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser, WebhookHanlder
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+# Create your views here.
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+
+# Define Receiver
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+        
+        # TODO: Handler when receiver Line Message
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                if isinstance(event.message, TextMessage):
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=event.message.text)
+                    )
+
+
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
