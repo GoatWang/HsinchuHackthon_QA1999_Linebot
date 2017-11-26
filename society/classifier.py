85e749e2c87b5eb33fea0e7c072a6c0ee1db24b6
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-26 12:42:02 +0800

    test classfier

--- society/classifier.py
+++ society/classifier.py
@@ -2,7 +2,6 @@ from django.conf import settings
 
 import jieba
 import os
-jieba.set_dictionary(os.path.join('society', 'models', 'dict.txt.big'))
 
 from pymongo import MongoClient
 import pandas as pd
@@ -18,12 +17,21 @@ import numpy as np
 class Classifier():
 
     def __init__(self):
-        with open(os.path.join('society', 'models', 'cat_mapping'), 'r' , encoding='utf8') as f:
+        if __name__ == "__main__":
+            ModelsDir = 'models'
+        else:
+            ModelsDir = os.path.join('society', 'models')
+
+        jieba.set_dictionary(os.path.join(ModelsDir, 'dict.txt.big'))
+        with open(os.path.join(ModelsDir, 'cat_mapping'), 'r' , encoding='utf8') as f:
             self.cat_mapping = json.load(f)
-        with open(os.path.join('society', 'models', 'vectorterms'), 'r' , encoding='utf8') as f:
+        with open(os.path.join(ModelsDir, 'vectorterms'), 'r' , encoding='utf8') as f:
             self.vectorterms = json.load(f)
-        with open(os.path.join('society', 'models', 'contactinfo.json'), 'r', encoding='utf8') as f:
+        with open(os.path.join(ModelsDir, 'contactinfo.json'), 'r', encoding='utf8') as f:
             self.cat_contact_mapping = json.load(f)
+
+        self.bst = xgb.Booster({'nthread': 4})  # init model
+        self.bst.load_model(os.path.join(ModelsDir, '20171125 232430246178.model'))  # load data
 
 
     def getcat_mapping(self):
@@ -33,9 +41,6 @@ class Classifier():
         return self.vectorterms
 
     def predict_cat(self, test_sentence):
-        bst = xgb.Booster({'nthread': 4})  # init model
-        bst.load_model(os.path.join('society', 'models', '20171125 232430246178.model'))  # load data
-
         # print(test_sentence)
         words = list(jieba.cut(test_sentence, cut_all=False))
         # print(", ".join(words))
@@ -56,7 +61,7 @@ class Classifier():
                 self_main_list[idx] += 1
             
         vector = self_main_list
-        cat_num = bst.predict(xgboost.DMatrix(np.array([vector,])))[0]
+        cat_num = self.bst.predict(xgboost.DMatrix(np.array([vector,])))[0]
         # print(cat_num)
 
         cat = None
@@ -73,22 +78,23 @@ class Classifier():
 
 if __name__ == "__main__":
     clf = Classifier()
+    cat = clf.predict_cat("兒童")
+    print(cat)
+    # def getall(col_name):
+    #     conn = MongoClient()
+    #     db = conn.QA1999
+    #     collection = db[col_name]
+    #     cursor = collection.find({})
+    #     rows = [row for row in cursor]
+    #     df = pd.DataFrame(rows)
+    #     return df
 
-    def getall(col_name):
-        conn = MongoClient()
-        db = conn.QA1999
-        collection = db[col_name]
-        cursor = collection.find({})
-        rows = [row for row in cursor]
-        df = pd.DataFrame(rows)
-        return df
+    # df_test= getall('taipei')[:100]
+    # idxs = np.random.choice(df_test.index, size=5)
+    # test_sentences = df_test.loc[idxs, 'question']
 
-    df_test= getall('taipei')[:100]
-    idxs = np.random.choice(df_test.index, size=5)
-    test_sentences = df_test.loc[idxs, 'question']
-
-    for sen in test_sentences:
-        cat = clf.predict_cat(sen)
-        print(sen)
-        print(cat)
-        print("======")
+    # for sen in test_sentences:
+    #     cat = clf.predict_cat(sen)
+    #     print(sen)
+    #     print(cat)
+    #     print("======")
