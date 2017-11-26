6f397ba97a6b4ce3dbde38e7dfde489f19c520b7
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-26 08:35:35 +0800

    society url

--- society/views.py
+++ society/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-from linebot import LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookParser ##, WebhookHanlder
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 # Create your views here.
@@ -11,37 +11,31 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
 # Define Receiver
-handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
-
-
-@handler.default()
-def default(event):
-    print(event)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='Currently Not Support None Text Message')
-    )
-
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
-
+        
+        # TODO: Handler when receiver Line Message
         try:
-            handler.handle(body, signature)
+            events = parser.parse(body, signature)
         except InvalidSignatureError:
             return HttpResponseForbidden()
         except LineBotApiError:
             return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                if isinstance(event.message, TextMessage):
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=event.message.text)
+                    )
+
+
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
