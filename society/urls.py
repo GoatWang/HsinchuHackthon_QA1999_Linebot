8fa469d486a5f567c4c99a6c21e1d4a76f9d7572
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-26 13:37:11 +0800

    url

--- society/urls.py
+++ society/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url('^index/', views.index),
     url('^callback/', views.callback),
-    url(r'^webcallback/(?P<query>.+)/$', views.webcallback),
+    url(r'^webcallback(?P<query>.+)/$', views.webcallback),
 ]
