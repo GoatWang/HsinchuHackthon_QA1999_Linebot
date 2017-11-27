b0a141a8621d4439c32de6d268b732ef32b89343
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-27 14:03:52 +0800

    testing

--- society/views.py
+++ society/views.py
@@ -37,20 +37,20 @@ def _handle_text_msg(event, relatedrows, contactinfo, feedbackstring):
         template=ButtonsTemplate(
             title='您是否想問以下問題?',
             text= feedbackstring,
-            actions = [
-                PostbackTemplateAction(label="1. " + questions[0][:5], text=answers[0][:200], data='buttonfeedback=True'),
-                PostbackTemplateAction(label="2. " + questions[1][:5], text=answers[1][:200], data='buttonfeedback=True'),
-                PostbackTemplateAction(label="3. " + questions[1][:5], text=answers[2][:200], data='buttonfeedback=True'),
-                PostbackTemplateAction(label="皆不是以上問題!", text=contactinfo[:300], data='buttonfeedback=True')
-            ]
-            # actions=[
-            #     MessageTemplateAction(label='message', text='message text'),
-            #     MessageTemplateAction(label='message', text='message text'),
-            #     MessageTemplateAction(label='message', text='message text'),
-            #     MessageTemplateAction(label='message', text='message text'),
-            #     # PostbackTemplateAction(label='postback', text='postback text', data='action=buy&itemid=1'),
-            #     # URITemplateAction(label='uri', uri='http://example.com/')
+            # actions = [
+            #     PostbackTemplateAction(label="1. " + questions[0][:5], text=answers[0][:200], data='buttonfeedback=True'),
+            #     PostbackTemplateAction(label="2. " + questions[1][:5], text=answers[1][:200], data='buttonfeedback=True'),
+            #     PostbackTemplateAction(label="3. " + questions[1][:5], text=answers[2][:200], data='buttonfeedback=True'),
+            #     PostbackTemplateAction(label="皆不是以上問題!", text=contactinfo[:300], data='buttonfeedback=True')
             # ]
+            actions=[
+                PostbackTemplateAction(label="皆不是以上問題!", text=contactinfo[:300], data='buttonfeedback=True'),
+                MessageTemplateAction(label='message', text='message text'),
+                MessageTemplateAction(label='message', text='message text'),
+                MessageTemplateAction(label='message', text='message text'),
+                # PostbackTemplateAction(label='postback', text='postback text', data='action=buy&itemid=1'),
+                # URITemplateAction(label='uri', uri='http://example.com/')
+            ]
         )
     )
     
@@ -105,7 +105,6 @@ def callback(request, buttonfeedback=False):
                         cat = clf.predict_cat()
                         contactinfo = clf.getcontactinfo(cat)
                         relatedrows = clf.findsimilar()
-
                         feedbackstring = clf.getfeedbackinfo(cat, relatedrows)
                         # line_bot_api.reply_message(
                         #     event.reply_token,
